1e47a5d965669bb2b142af1d63f4fabf128c8682
Rodrigo Pereira <rodrigorpogo@gmail.com>
2019-02-08 04:02:54 -0200

    Almost finished blog-app

--- Projects/blog-app/app.js
+++ Projects/blog-app/app.js
@@ -1,7 +1,8 @@
 const express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    moment = require('moment');
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,72 +17,117 @@ const schema = new mongoose.Schema({
     title: String,
     image: String,
     body: String,
-    created: {type: Date, default: Date.now}
+    created: { type: Date, default: Date.now }
 });
 
 const Dog = mongoose.model('Dog', schema);
 
+const findDogById = (id) => {
+    return Dog.findById(id, (err, result) => {
+        if (!err && result) {
+        }
+    });
+}
+
+const findDogs = () => {
+    return Dog.find({}, (err, result) => {
+        if (!err && result) {
+        }
+    });
+}
+
 const createDog = (dogObject) => {
-    Dog.create(dogObject, (err, result) => {
+    return Dog.create(dogObject, (err, result) => {
         if (!err && result) {
         }
     });
 }
 
 const updateDog = (id, dogObject) => {
-    Dog.update({ _id: id }, dogObject, (err, result) => {
+    return Dog.update({ _id: id }, dogObject, (err, result) => {
         if (!err && result) {
         }
     });
 }
 
 const deleteDog = (id) => {
-    Dog.deleteOne({ _id: id }, (err, result) => {
+    return Dog.deleteOne({ _id: id }, (err, result) => {
         if (!err && result) {
         }
     });
 }
 
+// MOCK
+
+const dogsMock = [{
+    image: "https://cdn1.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg", title: "Dogs",
+    body: "Dogs body loren", created: moment().format('Do MMMM YYYY')
+},
+{
+    image: "https://cdn1.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg", title: "Dogs",
+    body: "Dogs body", created: moment().format('Do MMMM YYYY')
+}];
+dogsMock[0]['_id'] = 1;
+dogsMock[1]['_id'] = 2;
+
 // Default
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.render("index");
 });
 
 // Show all Dogs
-app.get("/dogs", function(req, res){
-    res.render("show-all-dogs");
+app.get("/dogs", function (req, res) {
+    findDogs().then((data) => {
+        res.render("show-all-dogs", { dogs: data });
+    });
 });
 
 // Show new dog form
-app.get("/dogs/new", function(req, res){
+app.get("/dogs/new", function (req, res) {
     res.render("new-dog");
 });
 
 // Create new dog
-app.post("/dogs", function(req, res){
-    res.redirect("show-all-dogs");
+app.post("/dogs", function (req, res) {
+    createDog(req.body).resolve("Success").then(
+        findDogs().then((data) => {
+            res.render("show-all-dogs", { dogs: data });
+        })
+    )
 });
 
 // Show info about one specific dog
-app.get("/dogs/:id", function(req, res){
-    res.render("show-dog");
+app.get("/dogs/:id", function (req, res) {
+    findDogById(req.params.id).then((data) => {
+        res.render("show-dog", { dog: data });
+    });
 });
 
 // Show edit form for one dog
-app.get("/dogs/:id/edit", function(req, res){
-    res.render("index");
+app.get("/dogs/:id/edit", function (req, res) {
+    findDogById(req.params.id).then((data) => {
+        res.render("edit-dog", { dog: data });
+    });
 });
 
-// Show edit form for one dog
-app.put("/dogs/:id", function(req, res){
-    res.redirect("edit-dog");
+// Edit a dog
+app.put("/dogs/:id", function (req, res) {
+    updateDog(req.params.id, req.body).then(
+        findDogs().then((data) => {
+            res.render("show-all-dogs", { dogs: data });
+        })
+    );
 });
 
 // Delete a particular dog, then redirect home
-app.delete("/dogs/:id", function(req, res){
-    res.redirect("show-all-dogs");
+app.delete("/dogs/:id", function (req, res) {
+    deleteDog(req.params.id).then(
+        findDogs().then((data) => {
+            res.render("show-all-dogs", { dogs: data });
+        })
+    );
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server started!");
 });
